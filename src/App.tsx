acadb6d1178ad4c844ebb566206da585ea376ee6
David Christian <136609271+Myaneeeee@users.noreply.github.com>
2025-05-31 10:58:52 +0700

    remove loading

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,6 @@ import DashboardPage from "./pages/Dashboard/page";
 
 import { AuthProvider } from "./context/auth-context";
 
-
 // Loading Screen Animation
 
 const LoadingScreen = ({ onComplete }: { onComplete: () => void }) => {
@@ -74,7 +73,8 @@ const LoadingScreen = ({ onComplete }: { onComplete: () => void }) => {
 };
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   return (
     // Providers, Router, Scroll to Top Function and Button, and Custom Cursor
@@ -94,7 +94,6 @@ function App() {
               {/* <Route path="*" element={<NotFoundPage />} /> */}
 
               <Route path="/" element={<Layout />}>
-              
                 <Route index element={<LandingPage />} />
                 <Route path="/team" element={<TeamPage />} />
                 <Route path="/contact" element={<ContactPage />} />
@@ -106,33 +105,55 @@ function App() {
                 <Route path="/article/:id" element={<ArticleDetailPage />} />
 
                 <Route path="/marketplace" element={<MarketplacePage />} />
-                <Route path="/marketplace/:id" element={<MarketplaceDetailPage />} />
+                <Route
+                  path="/marketplace/:id"
+                  element={<MarketplaceDetailPage />}
+                />
 
                 <Route path="/forum" element={<ForumPage />} />
 
                 <Route path="/features" element={<FeaturesPage />} />
 
                 <Route path="/chatbot" element={<ChatbotPage />} />
-                <Route path="/disease-detection" element={<DiseaseDetectionPage />} />
+                <Route
+                  path="/disease-detection"
+                  element={<DiseaseDetectionPage />}
+                />
 
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
-
               </Route>
 
-              <Route path="/dashboard" element={<DashboardPage/>} />
-              <Route path="/dashboard/livestock" element={<DashboardLivestockPage/>} />
-              <Route path="/dashboard/livestock/add" element={<AddLivestockPage/>} />
-              <Route path="/dashboard/livestock/:id" element={<DashboardLivestockDetailPage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route
+                path="/dashboard/livestock"
+                element={<DashboardLivestockPage />}
+              />
+              <Route
+                path="/dashboard/livestock/add"
+                element={<AddLivestockPage />}
+              />
+              <Route
+                path="/dashboard/livestock/:id"
+                element={<DashboardLivestockDetailPage />}
+              />
 
-              <Route path="/dashboard/health" element={<DashboardHealthPage/>} />
-              <Route path="/dashboard/feeding" element={<DashboardFeedingPage/>} />
-              <Route path="/dashboard/analytics" element={<DashboardAnalyticsPage/>} />
+              <Route
+                path="/dashboard/health"
+                element={<DashboardHealthPage />}
+              />
+              <Route
+                path="/dashboard/feeding"
+                element={<DashboardFeedingPage />}
+              />
+              <Route
+                path="/dashboard/analytics"
+                element={<DashboardAnalyticsPage />}
+              />
 
-              <Route path="/dashboard/weather" element={<WeatherPage/>} />
+              <Route path="/dashboard/weather" element={<WeatherPage />} />
 
               <Route path="*" element={<NotFoundPage />} />
-
             </Routes>
           )}
         </AnimatePresence>
